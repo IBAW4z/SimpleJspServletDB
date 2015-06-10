205e72591fe0d993f1a229f55d56a2e414eb80c7
Anna <annasmo@student.agh.edu.pl>
2015-06-10 19:30:36 +0200

    klasa składnikowProduct

---
+++ SimpleJspServletDB/src/com/daniel/model/Product.java
@@ -0,0 +1,28 @@
+package com.daniel.model;
+
+/**
+ * File module, class Product.java
+ * class of products used in Recipe or present in Fridge
+ * Created by Anna on 2015-06-10.
+ */
+public class Product {
+    private String name;
+    private int amount;
+    //dorobić pochodne klasy dla róznych typów składników gdzie ilość
+    // będzie zależeć od typu składnika np. ml dla płynów,
+    // gramy dla sypkich ???
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    public String getAmount() {
+        return amount;
+    }
+
+}
