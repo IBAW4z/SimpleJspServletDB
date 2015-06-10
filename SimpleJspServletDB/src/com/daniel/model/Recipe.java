f48438e3532b8a3eb097ae1fd620ab16084d36d9
Anna <annasmo@student.agh.edu.pl>
2015-06-10 20:29:40 +0200

    klasa przepisow Recipe

--- SimpleJspServletDB/src/com/daniel/model/Recipe.java
+++ SimpleJspServletDB/src/com/daniel/model/Recipe.java
@@ -9,13 +9,13 @@ import java.util.ArrayList;
 
 enum e_type{
     SOUP, DESSERT, SALAD, STARTER, MAIN_COURSE, DRINK
-};
+}
 enum e_flavour{
     SWEET, BITTER, SALTY, SOUR
-};
+}
 enum e_difficulty{
     EASY, MEDIUM, HARD
-};
+}
 
 public class Recipe {
     private String name;
@@ -28,26 +28,40 @@ public class Recipe {
 
     public void setName(String name) {
         this.name = name;
-    };
+    }
     public String getName() {
         return name;
-    };
+    }
     public void setType(e_type type) {
         this.type = type;
-    };
-    public String getType() {
+    }
+    public e_type getType() {
         return type;
-    };
+    }
     public void setFlavour(e_flavour flavour) {
         this.flavour = flavour;
-    };
-    public String getFlavour() {
+    }
+    public e_flavour getFlavour() {
         return flavour;
-    };
+    }
     public void setDifficulty(e_difficulty difficulty) {
         this.difficulty = difficulty;
-    };
-    public String getType() {
-        return type;
-    };
+    }
+    public e_difficulty getDifficulty() {
+        return difficulty;
+    }
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+    /*public void addProducts(Product product){
+      this.products.add(product) ;
+    }*/
+    @Override
+    public String toString() {
+        return "Recipe [Name=" + name + "products=" + products + ", type=" + type
+                + ", flavour=" + flavour + ", difficulty level=" + difficulty + "]";
+    }
 }
